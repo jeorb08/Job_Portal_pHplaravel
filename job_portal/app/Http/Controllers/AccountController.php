70b9ac469a42cb031b8f4fae18b1bdaf3a3ecebb
Jannatul Jeor Bushra <jjeorb08@gmail.com>
2025-08-11 00:43:27 +0600

    Authenticate

--- job_portal/app/Http/Controllers/AccountController.php
+++ job_portal/app/Http/Controllers/AccountController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
@@ -44,5 +45,33 @@ class AccountController extends Controller
     public function login(){
         
         return view('front.account.login');
+    }
+    public function authenticate(Request $request){
+        $validator= Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password'=> 'required',
+
+        ]);
+        if ($validator->passes()) {
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('account.profile');
+            } else {
+                return redirect()->route('account.login')->with('error','Either Email/Password is incorrect');
+            }
+            
+            
+        } else {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+
+    }
+    public function profile(){
+        return view('front.account.profile');
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('account.login');
     }
 }
--- job_portal/routes/web.php
+++ job_portal/routes/web.php
@@ -12,4 +12,7 @@ Route::get('/',[HomeController::class, 'index'])->name('home');
 Route::get('/account/register',[AccountController::class, 'registration'])->name('account.register');
 Route::post('/account/process-register',[AccountController::class, 'processRegistration'])->name('account.processRegisration');
 Route::get('/account/login',[AccountController::class, 'login'])->name('account.login');
+Route::post('/account/authenticate',[AccountController::class, 'authenticate'])->name('account.authenticate');
+Route::get('/account/profile',[AccountController::class, 'profile'])->name('account.profile');
+Route::get('/account/logout',[AccountController::class, 'logout'])->name('account.logout');
 
